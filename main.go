1519762860985a30df91e6fac762521fe482445b
mrtrkmn <mr.turkmen@icloud.com>
2022-03-02 20:34:26 +0100

    remove zip operation from main code
    Signed-off-by: mrtrkmn <mr.turkmen@icloud.com>

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"archive/zip"
 	"bufio"
 	"crypto/tls"
 	"crypto/x509"
@@ -12,7 +11,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 	"sync"
 	"time"
@@ -36,80 +34,6 @@ type zipFile struct {
 	name string
 	path string
 	url  string
-}
-
-func downloadZip(f zipFile) {
-	resp, _ := http.Get(f.url)
-	defer resp.Body.Close()
-	fileName := f.path + f.name
-	out, _ := os.Create(fileName)
-	defer out.Close()
-	io.Copy(out, resp.Body)
-}
-
-// taken from: https://stackoverflow.com/questions/20357223/easy-way-to-unzip-file-with-golang
-func unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	os.MkdirAll(dest, 0755)
-
-	// Closure to address file descriptors issue with all the deferred .Close() methods
-	extractAndWriteFile := func(f *zip.File) error {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if err := rc.Close(); err != nil {
-				panic(err)
-			}
-		}()
-
-		path := filepath.Join(dest, f.Name)
-
-		// Check for ZipSlip (Directory traversal)
-		if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
-			return fmt.Errorf("illegal file path: %s", path)
-		}
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(path, f.Mode())
-		} else {
-			os.MkdirAll(filepath.Dir(path), f.Mode())
-			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					panic(err)
-				}
-			}()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
-	for _, f := range r.File {
-		err := extractAndWriteFile(f)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 // NewBufferedWriteCloser creates an io.WriteCloser from a bufio.Writer and an io.Closer
@@ -144,7 +68,6 @@ func readURLs(dataPath string) [][]string {
 }
 
 func main() {
-	top_1_million := "https://statvoo.com/dl/top-1million-sites.csv.zip"
 
 	quiet := flag.Bool("q", true, "don't print the data")
 	keyLogFile := flag.String("keylog", "keylog.file", "key log file")
@@ -154,16 +77,7 @@ func main() {
 		sites: map[string]string{},
 	}
 	currentTime := time.Now().Format("2006-01-02")
-	zFile := zipFile{
-		name: fmt.Sprintf("top-1million-sites-%s.csv.zip", currentTime),
-		path: "data/",
-		url:  top_1_million,
-	}
-	downloadZip(zFile)
-	if err := unzip(zFile.path+zFile.name, zFile.path); err != nil {
-		fmt.Printf("Err %v", err)
-	}
-	// file top-1m.csv will be created and deleted as last step
+
 	for _, url := range readURLs("./data/top-1m.csv")[1:] {
 		urls = append(urls, fmt.Sprintf("https://www.%s", url[1]))
 	}
@@ -254,5 +168,4 @@ func main() {
 		noQUICFile.WriteString(fmt.Sprintf("%s , %s \n", k, v))
 	}
 	noQUICFile.Close()
-	os.Remove("./data/top-1m.csv")
 }
